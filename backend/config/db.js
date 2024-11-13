c88d80ed81047bbc6e0768fcccd56421082d6bf1
sriram5N2 <139240678+sriram5N2@users.noreply.github.com>
2024-11-13 09:51:52 +0530

    Delete backend directory

--- backend/config/db.js
+++
@@ -1,5 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://sriramt234:SRIRAM1234@backendb.h3v58.mongodb.net/?retryWrites=true&w=majority&appName=BACKENDB').then(()=>console.log("DataBase Connected"));
-}
